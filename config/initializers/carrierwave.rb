bde0d42bdd4f1ce76ac36ea9313584a6c5eaf8fb
Charlie Fu <charlie.zhe.fu@gmail.com>
2019-07-30 20:08:55 +0000

    Lesson 24 video uploading

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,19 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
+  if Rails.env.development?
+    config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
+    config.root = '/home/vagrant/uploads_tmp/tmp'
+  end
+
   config.storage    = :aws
   config.aws_bucket = ENV["AWS_BUCKET"]
   config.aws_acl    = "public-read"
                           # required
   config.aws_credentials = {                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],
-    region:                ENV["AWS_REGION"]        # required
+    access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    secret_access_key: ENV["AWS_SECRET_KEY"],
+    region:            ENV["AWS_REGION"]        # required
   }
                 # required
 end
