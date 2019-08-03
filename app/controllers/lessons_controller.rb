3c4a55229c27cef985c5cc7bf851388c4308ccc6
Charlie Fu <charlie.zhe.fu@gmail.com>
2019-08-03 18:19:20 +0000

    lesson 28 permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_for_current_course
+
   def show
     
   end
@@ -8,5 +11,12 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
       @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def require_enrolled_for_current_course
+    @course = current_lesson.section.course
+    if current_user.enrolled_in?(@course) == false
+      redirect_to course_path(@course), alert: 'Please enroll first to view lessons'
+    end
   end
 end
