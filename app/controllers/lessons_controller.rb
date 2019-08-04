68415d69a0f812ebf1c8e53804436f783fb298c2
Charlie Fu <charlie.zhe.fu@gmail.com>
2019-08-04 22:57:21 +0000

    lesson 29 payments

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -10,7 +10,7 @@ class LessonsController < ApplicationController
 
   helper_method :current_lesson
   def current_lesson
-      @current_lesson ||= Lesson.find(params[:id])
+    @current_lesson ||= Lesson.find(params[:id])
   end
 
   def require_enrolled_for_current_course
